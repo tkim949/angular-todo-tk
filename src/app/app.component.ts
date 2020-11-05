abb315e265b338dfd6034e0b8c1a44654bef3b6c
tkim949 <tkim949@gmail.com>
2020-11-04 19:19:36 -0800

    first commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todoArray=[];
   title = 'todo-tk';
+  //add item
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+
+  //delete item
+  deleteItem(){   
+    console.log("delete item");  
+  }
+
+
 }
